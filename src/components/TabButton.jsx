0de105352c7b501383147b56208dd11e065cdfb1
nalmario <noahralmario@gmail.com>
2025-05-23 10:48:07 -0500

    App.jsx & TabButton.jsx - modified to support conditional CSS rendering of components

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,8 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     // event listener props
     <li>
-      <button onClick={onSelect} >{children}</button>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect} >{children}</button>
     </li> // 'children' contains anything that comes between component tags (in App.jsx)
   )
 }
