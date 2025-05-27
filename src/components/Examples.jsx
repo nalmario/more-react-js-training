ed876cfbaa9cf5e361112aa2049e22672114aa0b
nalmario <noahralmario@gmail.com>
2025-05-27 10:55:34 -0500

    created Tabs.jsx file to allow for multiple TabButton component 'slots'
    Examples.jsx - modified to put TabButton components into a Tabs prop

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EXAMPLES } from "../data.js";
 import TabButton from "./TabButton.jsx";
 import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 
 export default function Examples() {
   // cannot nest state hook in any other compfunction (top-level)
@@ -16,43 +17,47 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === 'components'} // conditional CSS rendering prop
-          onClick={() => handleSelect('components')} // event handling prop
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'jsx'}
-          onClick={() => handleSelect('jsx')}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'props'}
-          onClick={() => handleSelect('props')}
-        > Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'state'}
-          onClick={() => handleSelect('state')}
-        > State
-        </TabButton>
-      </menu>
-      {!selectedTopic ? ( // conditional rendering
-        <p>Please select a topic.</p>
-      ) : (
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-              {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-        </div>
-      )}
+      <Tabs
+        buttons = {
+          <>
+            <TabButton
+              isSelected={selectedTopic === 'components'} // conditional CSS rendering prop
+              onClick={() => handleSelect('components')} // event handling prop
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onClick={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onClick={() => handleSelect('props')}
+            > Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onClick={() => handleSelect('state')}
+            > State
+            </TabButton>
+          </>
+        }>
+        {!selectedTopic ? ( // conditional rendering
+          <p>Please select a topic.</p>
+        ) : (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
     </Section>
   );
 }
