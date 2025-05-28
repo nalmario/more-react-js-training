712e0742e1ce518f423cf93ff162132fb01ac9fe
nalmario <noahralmario@gmail.com>
2025-05-28 10:00:04 -0500

    Player.jsx - button onClick functionality and handler function added

--- react-deep-dive/src/components/Player.jsx
+++ react-deep-dive/src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" required />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
