e488e3eb87ee32d87a0df9fba18bc4392272d858
nalmario <noahralmario@gmail.com>
2025-05-28 10:52:56 -0500

    Player.jsx - modified component to change and update player name values using two-way binding
    App.jsx - modified Player component prop naming

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -5,8 +5,9 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          {/*two completely isolated instances of Player comoponent*/}
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
--- react-deep-dive/src/components/Player.jsx
+++ react-deep-dive/src/components/Player.jsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
+  // Handles edit/save button for changing player name
   function handleEditClick() {
-    setIsEditing(true);
+    // setIsEditing(!isEditing)
+    setIsEditing((editing) => !editing); // BEST PRACTICE: going back and forth between two states, use a function to ensure instant change (bts)
+  }
+
+  // Handles the change of the player name input when editing
+  function handleChange(event) {
+    setPlayerName(event.target.value); // two-way binding, event will change player name and feed it back into the input where it's called
   }
 
   return (
     <li>
       <span className="player">
         {!isEditing ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" required />
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          />
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
     </li>
   );
 }
