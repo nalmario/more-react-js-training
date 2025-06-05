feeb0ef54371268045fffe18427605a299f0258c
nalmario <noahralmario@gmail.com>
2025-06-05 13:40:30 -0500

    using refs to handle name change and display values

--- react-refs-portals/src/components/Player.jsx
+++ react-refs-portals/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  // Ref value will be used to get value put in input field
+  const playerNameInput = useRef();
+
+  // State of player name
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  // Handle button set name button click
+  function handleClick() {
+    setEnteredPlayerName(playerNameInput.current.value); // can access element properties like 'value'
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* do this if true ?? do this if false */}
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* note ref prop */}
+        <input ref={playerNameInput} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
