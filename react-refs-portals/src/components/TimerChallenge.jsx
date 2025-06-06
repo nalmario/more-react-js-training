a5eb36351c31ddf01bb7e227233fa87f23cbe230
nalmario <noahralmario@gmail.com>
2025-06-06 10:51:26 -0500

    modified TimerChallenge component to handle start of game timers

--- react-refs-portals/src/components/TimerChallenge.jsx
+++ react-refs-portals/src/components/TimerChallenge.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  // Handles start of game timer
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000); // Timer set in milliseconds
+
+    setTimerStarted(true);
+  }
+
+  // Handles end of game timer
+  function handleStop() {}
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost</p>}
       <p className="challenge-time">
-        {targetTime} seconds{targetTime > 1 ? "s" : ""}
+        {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 }
